29c0f6540e61bf5885061dfe1f9a1297335fe3eb
Michael Jones <proclivis@gmail.com>
2017-07-02 03:23:48 +0000

    Fix JavaScript indentation.

--- src/Data/UInt.js
+++ src/Data/UInt.js
@@ -99,48 +99,41 @@ exports.pow = function (u) {
 };
 
 exports.and = function (n1) {
-  return function (n2) {
-    /* jshint bitwise: false */
-    return (n1 & n2) >>> 0;
-  };
+    return function (n2) {
+        return (n1 & n2) >>> 0;
+    };
 };
 
 exports.or = function (n1) {
-  return function (n2) {
-    /* jshint bitwise: false */
-    return (n1 | n2) >>> 0;
-  };
+    return function (n2) {
+        return (n1 | n2) >>> 0;
+    };
 };
 
 exports.xor = function (n1) {
-  return function (n2) {
-    /* jshint bitwise: false */
-    return (n1 ^ n2) >>> 0;
-  };
+    return function (n2) {
+        return (n1 ^ n2) >>> 0;
+    };
 };
 
 exports.shl = function (n1) {
-  return function (n2) {
-    /* jshint bitwise: false */
-    return (n1 << n2) >>> 0;
-  };
+    return function (n2) {
+        return (n1 << n2) >>> 0;
+    };
 };
 
 exports.shr = function (n1) {
-  return function (n2) {
-    /* jshint bitwise: false */
-    return (n1 >> n2) >>> 0;
-  };
+    return function (n2) {
+        return (n1 >> n2) >>> 0;
+    };
 };
 
 exports.zshr = function (n1) {
-  return function (n2) {
-    /* jshint bitwise: false */
-    return (n1 >>> n2) >>> 0;
-  };
+    return function (n2) {
+        return (n1 >>> n2) >>> 0;
+    };
 };
 
 exports.complement = function (n) {
-  /* jshint bitwise: false */
     return (~n >>> 0);
 };
