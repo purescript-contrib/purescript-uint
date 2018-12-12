1cf6dd7095d3d012c1fb85edfe48872c5e144ab6
Athan Clark <athan.clark@gmail.com>
2018-12-12 07:44:43 -0700

    cosmetic

--- test/Main.purs
+++ test/Main.purs
@@ -12,12 +12,12 @@ newtype TestUInt = TestUInt UInt
 
 instance arbitraryTestUInt :: Arbitrary TestUInt where
   arbitrary = TestUInt <<< fromNumber <$> arbitrary
-derive newtype instance boundedTestUInt :: Bounded TestUInt  
+derive newtype instance boundedTestUInt :: Bounded TestUInt
 derive newtype instance eqTestUInt :: Eq TestUInt
-derive newtype instance ordTestUInt :: Ord TestUInt                        
+derive newtype instance ordTestUInt :: Ord TestUInt
 derive newtype instance semiringTestUInt :: Semiring TestUInt
 derive newtype instance ringTestUInt :: Ring TestUInt
-derive newtype instance commutativeRingTestUInt :: CommutativeRing TestUInt                        
+derive newtype instance commutativeRingTestUInt :: CommutativeRing TestUInt
 derive newtype instance euclideanRingTestUInt :: EuclideanRing TestUInt
 
 
@@ -26,10 +26,8 @@ main = do
   let prxUInt = Proxy ∷ Proxy TestUInt
   Data.checkEq prxUInt
   Data.checkOrd prxUInt
-  Data.checkSemiring prxUInt  
+  Data.checkSemiring prxUInt
   Data.checkRing prxUInt
   Data.checkEuclideanRing prxUInt
-  Data.checkBounded prxUInt    
+  Data.checkBounded prxUInt
   Data.checkCommutativeRing prxUInt
-  
-  
