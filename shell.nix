6546e5002e57b69c6b6b07869fc99afe9ade372b
James Brock <jamesbrock@gmail.com>
2021-05-20 14:28:53 +0900

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  easy-ps = import (builtins.fetchGit {
+    url = "https://github.com/justinwoo/easy-purescript-nix.git";
+    ref = "master";
+    rev = "dae91f43317fd5ff207e11ea6bf4b6130e4ba9fc";
+  }) { inherit pkgs; };
+in
+pkgs.mkShell {
+  buildInputs = [
+    easy-ps.purs-0_14_1
+    easy-ps.spago
+    easy-ps.pulp
+    pkgs.nodejs-14_x
+    pkgs.nodePackages.bower
+  ];
+  LC_ALL = "C.UTF-8"; # https://github.com/purescript/spago/issues/507
+}
